abb9d637baa23dc52ce7d4fa7d6c050d4e047ac3
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-12-13 18:33:10 +0300

    add appwrite support

--- stage4/backend/src/main.py
+++ stage4/backend/src/main.py
@@ -1,6 +1,16 @@
-from typing import Union
+from os import environ as env
 
+from appwrite.client import Client
+from dotenv import load_dotenv
 from fastapi import FastAPI
+
+load_dotenv("../../.env", verbose=True)
+
+client = Client()
+client.set_endpoint(f"https://{env.get('APPWRITE_REGION')}.cloud.appwrite.io/v1")
+client.set_project(env.get("APPWRITE_PROJECT_ID"))
+client.set_key(env.get("APPWRITE_API_KEY"))
+
 
 app = FastAPI()
 
