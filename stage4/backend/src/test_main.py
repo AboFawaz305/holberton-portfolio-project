0a4847453180ae50c8ff3870780c8498cfe9f46b
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-12-24 20:20:45 +0300

    change db to mongo

--- stage4/backend/src/test_main.py
+++ stage4/backend/src/test_main.py
@@ -1,6 +1,12 @@
 """Unit tests to test the API
 """
 
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
 
 def test_always_pass():
     """Make sure the testing suite work
