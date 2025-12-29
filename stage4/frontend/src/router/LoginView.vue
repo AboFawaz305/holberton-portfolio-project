b18c7f9f2fbc739635744f8167f1ae767d9c9cc6
ismailRowaihy <ismailRowaihy@gmail.com>
2025-12-29 23:03:11 +0300

    added login page

--- stage4/frontend/src/router/LoginView.vue
+++ stage4/frontend/src/router/LoginView.vue
@@ -5,8 +5,14 @@ export default {
   data() {
     return {
       loginFormSchema: object({
-        username: string().required().min(3).max(25),
-        password: string().required().min(8).max(50),
+        username: string()
+        .required("هذا الحقل الزامي")
+        .min(3 ,"لا يمكن  ان يكون اقل من 3 احرف")
+        .max(25,"يجب ان لا يكون اكثر من 25 حرفا"),
+        password: string()
+        .required("هذا الحقل الزامي")
+        .min(8 ," لا يمكن ان يكون اقل من 8 احرف")
+        .max(50,"يجب ان لا يكون اكثر من 50 حرفا"),
       }),
       loginErrorMessage: '',
       loginSucccessMessage: '',
@@ -31,7 +37,7 @@ export default {
       if (!response.ok) {
         const error = await response.json()
         this.loginSucccessMessage = ''
-        this.loginErrorMessage = error.detail
+        this.loginErrorMessage = "خطا يا"
         return
       }
 
@@ -39,24 +45,97 @@ export default {
 
       localStorage.setItem('token', token.access_token)
       this.loginErrorMessage = ''
-      this.loginSucccessMessage = 'Login done sucessfully'
+      this.loginSucccessMessage = 'تم تسجيل الدخل بنجاح'
       this.$router.push('/')
     },
   },
 }
 </script>
 <template>
+  <div class="contanor">
   <Form @submit="onSubmit" :validation-schema="loginFormSchema">
-    <p v-if="loginErrorMessage.length">{{ loginErrorMessage }}</p>
-    <p v-if="loginSucccessMessage.length">{{ loginSucccessMessage }}</p>
 
-    <label for="username"> Username</label>
-    <Field name="username" />
-    <ErrorMessage name="username" />
+  <h1>تسجيل الدخول</h1>
+  <p v-if="loginErrorMessage.length" class="error">{{ loginErrorMessage }}</p>
+  <p v-if="loginSucccessMessage.length">{{ loginSucccessMessage }}</p>
+  
+  <label for="username"> اسم المستخدم </label>
+  <ErrorMessage class='error' name="username" />
+  <Field name="username" />
+  
+  
+  <label for="password">كلمة المرور</label>
+  <ErrorMessage class='error' name="password" />
+  <Field name="password" type="password" />
+  <div class='forgot' >
+    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"> هل نسيت كلمة المرور؟ </a>
+  </div>
+  
+  
+  <button type="submit">تسيجل الدخول</button>
 
-    <label for="password">Password</label>
-    <Field name="password" type="password" />
-    <ErrorMessage name="password" />
-    <button type="submit">Login</button>
+  <div class="noAccount">
+    <RouterLink  to="/register">ليس لديك حساب ؟ أنشاء حساب</RouterLink>
+  </div>
+
   </Form>
+  </div>
 </template>
+<style scoped>
+.contanor{
+  min-height: 100%;
+  background-color: #5dadbb;
+}
+
+.forgot{
+  display: flex;
+  justify-content:flex-end;
+}
+.noAccount{
+  font-weight: bolder;
+  display: flex;
+  justify-content: center;
+}
+h1{
+  text-align: center;
+}
+.error{
+  color: red;
+}
+form{
+  
+  display: flex;
+  justify-content:flex-end;
+  flex-direction: column;
+  max-width: 400px;
+  margin: auto;
+  gap:20px;
+  border-radius: 15px;
+  border: 2px solid #5dadbb;
+  padding: 5em;
+  background-color: #e2e2e2;
+}
+label {
+  font-size: 1.5em;
+  font-weight: bolder;
+}
+input {
+
+  margin-bottom: 2em;
+  padding: 0.5em;
+  border-radius: 5px;
+  border: 2px solid #5dadbb;
+}
+
+form button {
+  color:white;
+  font-size: 1.25em;
+  margin-top: 2rem;
+  margin-inline: 12.5%;
+  border-radius: 5px;
+  border: 2px solid #5dadbb;
+  background-image: linear-gradient(to right, #5dadbb, #294f58)
+}
+
+
+</style>
