b809d3b54f17eed6e8098c93cd76927f48c81f98
Farah <farah.dev@icloud.com>
2026-01-07 21:53:49 +0300

    implement main landing page

--- stage4/frontend/src/router/index.ts
+++ stage4/frontend/src/router/index.ts
@@ -5,6 +5,7 @@ import LoginView from './LoginView.vue'
 import OrganizationsView from './OrganizationsView.vue'
 import OrgView from './OrganizationChatView.vue'
 import UserProfileView from './UserProfileView.vue'
+import MainView from './MainView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,8 @@ const router = createRouter({
     { path: '/organizations', component: OrganizationsView },
     { path: '/organizations/:id', component: OrgView, props: true },
     { path: '/profile', component: UserProfileView },
+    { path: '/home', component: MainView },
+    { path: '/', redirect: '/home' },
   ],
 })
 
