f80e94db26046a56c49eda6795385708e9c67d4d
Farah <farah.dev@icloud.com>
2026-01-14 01:22:17 +0300

    Add NewGroupData model and finalize group creation endpoint

---
+++ stage4/backend/src/core/NewGroupData.py
@@ -0,0 +1,12 @@
+"""NewGroupData model
+"""
+from pydantic import BaseModel, Field
+
+
+class NewGroupData(BaseModel, extra="forbid"):
+    """The representation of a group
+    Extra fields are not permitted
+    """
+    title: str   = Field(min_length=3, max_length=100)
+    parent_group_id: Optional[str] = None
+
