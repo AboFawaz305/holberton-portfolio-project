905b56efd28e4d29f26556bf79ff82115a6489a8
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2026-01-13 19:51:09 +0300

    fix pylint

--- stage4/backend/src/spam_model.py
+++ stage4/backend/src/spam_model.py
@@ -1,7 +1,11 @@
+"""Provide methods to deal with spam messages
+"""
 from profanityfilter import ProfanityFilter
 
 pf = ProfanityFilter()
 
 
 def is_spam(message: str) -> bool:
+    """Return true if the message is considered a spam
+    """
     return pf.is_profane(message)
